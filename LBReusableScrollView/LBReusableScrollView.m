1316933848d012de215592126349a597bea014d0
刘彬 <1129434577@qq.com>
2019-10-11 15:55:02 +0800

    no message

--- LBReusableScrollView/LBReusableScrollView.m
+++ LBReusableScrollView/LBReusableScrollView.m
@@ -32,6 +32,9 @@
     self.delegate = self;
     [self reloadData];
 }
+-(void)setDelegate:(id<UIScrollViewDelegate>)delegate{
+    [super setDelegate:self];
+}
 
 -(void)setCurrentPage:(NSUInteger)currentPage{
     _currentPage = currentPage;
